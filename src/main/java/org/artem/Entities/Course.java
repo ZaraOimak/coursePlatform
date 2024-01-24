0ccfaf67567a7eeaa0029b596de4bb8fba1fdc69
Artem Osipov <emaildlyaege@gmail.com>
2024-01-24 15:54:33 +0300

    create Entities

---
+++ src/main/java/org/artem/Entities/Course.java
@@ -0,0 +1,57 @@
+package org.artem.Entities;
+public class Course {
+    private Integer idCourse;
+    private String nameCourse;
+    private String descriptionCourse;
+    private String nameAuthor;
+    private Section section;
+    private Author author;
+
+    public Integer getIdCourse() {
+        return idCourse;
+    }
+
+    public void setIdCourse(Integer idCourse) {
+        this.idCourse = idCourse;
+    }
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    public String getDescriptionCourse() {
+        return descriptionCourse;
+    }
+
+    public void setDescriptionCourse(String descriptionCourse) {
+        this.descriptionCourse = descriptionCourse;
+    }
+
+    public String getNameAuthor() {
+        return nameAuthor;
+    }
+
+    public void setNameAuthor(String nameAuthor) {
+        this.nameAuthor = nameAuthor;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/org/artem/Entities/LinkToTheme.java
@@ -0,0 +1,30 @@
+package org.artem.Entities;
+public class LinkToTheme {
+    private Integer idTheme;
+    private String prevLink;
+    private String nextLink;
+
+    public Integer getIdTheme() {
+        return idTheme;
+    }
+
+    public void setIdTheme(Integer idTheme) {
+        this.idTheme = idTheme;
+    }
+
+    public String getPrevLink() {
+        return prevLink;
+    }
+
+    public void setPrevLink(String prevLink) {
+        this.prevLink = prevLink;
+    }
+
+    public String getNextLink() {
+        return nextLink;
+    }
+
+    public void setNextLink(String nextLink) {
+        this.nextLink = nextLink;
+    }
+}
